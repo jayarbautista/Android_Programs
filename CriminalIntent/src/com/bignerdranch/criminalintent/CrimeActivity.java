de69616b9542e620833dbcee5f8fa80fa1cf74dc
jayarbautista <j.bautista@klab.com>
2014-08-06 16:28:56 +0800

    Implementing newInstance(UUID)

--- CriminalIntent/src/com/bignerdranch/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/com/bignerdranch/criminalintent/CrimeActivity.java
@@ -1,10 +1,13 @@
 package com.bignerdranch.criminalintent;
 
+import java.util.UUID;
+
 import android.support.v4.app.Fragment;
 
 public class CrimeActivity extends SingleFragmentActivity {
 	@Override
-	protected Fragment createFragment() {
-		return new CrimeFragment();
-	}
+    protected Fragment createFragment() {
+		UUID crimeId = (UUID)getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+		return CrimeFragment.newInstance(crimeId);
+    }
 }
