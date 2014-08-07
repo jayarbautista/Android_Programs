4b73c5977d1ddaf6b9173e5d1fd2f456e875a736
jayarbautista <j.bautista@klab.com>
2014-08-07 16:47:16 +0800

    Added: Retaining Fragment and Localization

--- HelloMoon/gen/com/bignerdranch/hellomoon/R.java
+++ HelloMoon/gen/com/bignerdranch/hellomoon/R.java
@@ -1258,6 +1258,7 @@ containing a value of this type.
         public static final int app_name=0x7f0b000d;
         public static final int hello_world=0x7f0b000e;
         public static final int hellomoon_description=0x7f0b0014;
+        public static final int hellomoon_image_description=0x7f0b0015;
         public static final int hellomoon_pause=0x7f0b0011;
         public static final int hellomoon_play=0x7f0b0010;
         public static final int hellomoon_stop=0x7f0b0012;
--- HelloMoon/src/com/bignerdranch/hellomoon/HelloMoonFragment.java
+++ HelloMoon/src/com/bignerdranch/hellomoon/HelloMoonFragment.java
@@ -17,6 +17,13 @@ public class HelloMoonFragment extends Fragment {
 	private Button mVideoButton;
 	
 	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+	}
+
+	
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
 
