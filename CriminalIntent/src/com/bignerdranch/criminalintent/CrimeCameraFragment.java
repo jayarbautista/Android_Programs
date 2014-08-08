9ce2a7fbfea670d9315eb946189ff3a59b63615e
jayarbautista <j.bautista@klab.com>
2014-08-08 17:22:05 +0800

    Taking Pictures and Handling Images(Part 1)

--- CriminalIntent/src/com/bignerdranch/criminalintent/CrimeCameraFragment.java
+++ CriminalIntent/src/com/bignerdranch/criminalintent/CrimeCameraFragment.java
@@ -1,9 +1,12 @@
 package com.bignerdranch.criminalintent;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.hardware.Camera;
 import android.hardware.Camera.Size;
 import android.os.Build;
@@ -24,6 +27,47 @@ public class CrimeCameraFragment extends Fragment {
     private SurfaceView mSurfaceView;
     private View mProgressContainer;
 
+    private Camera.ShutterCallback mShutterCallback = new Camera.ShutterCallback() {
+		
+		public void onShutter() {
+			//Display the progress indicator
+			mProgressContainer.setVisibility(View.VISIBLE);
+			
+		}
+	};
+	
+	private Camera.PictureCallback mJpegCallback = new Camera.PictureCallback() {
+		
+		public void onPictureTaken(byte[] data, Camera camera) {
+			//Create a filename
+			String filename = UUID.randomUUID().toString() + ".jpg";
+			//Save the Jpeg data to disk
+			FileOutputStream os = null;
+			boolean success = true;
+			
+			try {
+				os = getActivity().openFileOutput(filename, Context.MODE_PRIVATE);
+				os.write(data);
+			} catch(Exception e) {
+				Log.e(TAG, "Error writing to file " +filename, e);
+				success = false;
+			} finally {
+				try {
+					if(os != null) {
+						os.close();
+					}
+				} catch(Exception e) {
+					Log.e(TAG, "Error closing file " + filename, e);
+					success = false;
+				}
+			}
+			
+			if(success) {
+				Log.i(TAG, "JPEG saved at " + filename);
+			}
+			getActivity().finish();
+		}
+	};
 
     @Override
     @SuppressWarnings("deprecation")
@@ -36,7 +80,9 @@ public class CrimeCameraFragment extends Fragment {
         Button takePictureButton = (Button)v.findViewById(R.id.crime_camera_takePictureButton);
         takePictureButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                getActivity().finish();
+                if(mCamera != null) {
+                	mCamera.takePicture(mShutterCallback, null, mJpegCallback);
+                }
             } 
         });
 
